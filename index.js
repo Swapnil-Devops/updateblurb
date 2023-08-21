5afc43de6dcf0b6c55b5210f99a052e83661c9d2
Swapnil Gawali <112171267+Swapnil-Devops@users.noreply.github.com>
2023-08-21 13:11:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 import * as core from "@actions/core";
 
 const accessToken = core.getInput("PAT"); 
-const discussionId = core.getInput("discussionId"); 
+const discussionId = core.getInput("discussionID"); 
 const updating_body = core.getInput("updatedbody");
 
 const graphqlMutation = `
@@ -19,15 +19,19 @@ const graphqlMutation = `
   }
 `;
 
-
+// Set up the GraphQL endpoint URL
 const apiUrl = 'https://api.github.com/graphql';
 
-
+// Set up headers for authentication and content type
 const headers = {
-  "Authorization": "Bearer ${accessToken}",
-  "Content-Type": "application/json",
+  Authorization: 'Bearer ${accessToken}',
+  'Content-Type': 'application/json',
 };
 
+// Make the GraphQL request using a POST request
+console.log('Api url',apiUrl)
+console.log('heade:',headers)
+console.log('graph mutation',graphqlMutation)
 fetch(apiUrl, {
   method: 'POST',
   headers: headers,
@@ -36,4 +40,7 @@ fetch(apiUrl, {
 .then(response => {
     if (response.status === 200) {
         console.log('Discussion updated successfully.');}
+    else{
+        console.log('Error:',response.status+response.message)
+    }
 });
