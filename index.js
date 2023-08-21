e19352a0b337f9950368712127d09fda8d42d4a4
Swapnil Gawali <112171267+Swapnil-Devops@users.noreply.github.com>
2023-08-21 22:12:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const graphqlMutation = `
   mutation {
     updateDiscussion(input: {
       discussionId: "${discussionId}",
-      body: ${updatingbody}
+      body: "${updatingbody}"
     }) {
       discussion {
         id
