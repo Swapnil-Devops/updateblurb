450324bd86dad612c87782feb1a78c5766f3c8ba
Swapnil Gawali <112171267+Swapnil-Devops@users.noreply.github.com>
2023-08-21 23:16:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
-import fetch from "node-fetch"; 
+import fetch from "node-fetch";
 import * as core from "@actions/core";
 
-const accessToken = core.getInput("PAT"); 
-const discussionId = "D_kwDOKCGw7s4AVF-r"; 
-const updatingbody = " GitHub Actions is a powerful automation tool provided by GitHub. It allows developers to define custom workflows and automate various tasks related to their software development process.";
-console.log('discussionId',discussionId);
-console.log('updatingbody',updatingbody);
+const accessToken = core.getInput("PAT");
+const discussionId = "D_kwDOKCGw7s4AVF-r";
+const updatingbody = "GitHub Actions is a powerful automation and continuous integration/continuous deployment platform provided by GitHub. It allows you to automate various tasks and workflows in your software development process. Here's an explanation of GitHub Actions: Automation: GitHub Actions enables you to automate various tasks and processes related to your software development projects. This automation can include code validation, testing, building, deployment, and more. Workflow-Based: GitHub Actions is based on the concept of workflows. A workflow is a series of steps or jobs that are executed when specific events occur in your GitHub repository, such as code pushes, pull requests, or issue creation.";
+console.log('discussionId', discussionId);
+console.log('updatingbody', updatingbody);
 
 const graphqlMutation = `
-  mutation {
-    updateDiscussion(input: {
-      discussionId: "${discussionId}",
-      body: "${updatingbody}"
-    }) {
-      discussion {
-        id
-        body
-      }
+    mutation {
+        updateDiscussion(input: {
+            discussionId: "${discussionId}",
+            body: "${updatingbody}"
+        }) {
+            discussion {
+                id
+                body
+            }
+        }
     }
-  }
 `;
 
-// Set up the GraphQL endpoint URL
 const apiUrl = 'https://api.github.com/graphql';
 
-// Set up headers for authentication and content type
+
 const headers = {
-  "Content-Type": "application/json",
-  'Authorization': 'Bearer '+ accessToken,
+    "Content-Type": "application/json",
+    'Authorization': 'Bearer '+ accessToken ,
 };
 
-// Make the GraphQL request using a POST request
-console.log('Api url',apiUrl)
-console.log('heade:',headers)
-console.log('graph mutation',graphqlMutation)
+
+console.log('Api url', apiUrl)
+console.log('heade:', headers)
+console.log('graph mutation', graphqlMutation)
 fetch(apiUrl, {
-  method: 'POST',
-  headers: headers,
-  body: JSON.stringify({ query: graphqlMutation }),
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify({ query: graphqlMutation }),
 })
-.then(response => {
-    if (response.status === 200) {
-        console.log('Discussion updated successfully.');}
-    else{
-        console.log('Error:',response.status+response.message)
-    }
-});
+    .then(response => {
+        if (response.status === 200) {
+            console.log('Discussion updated successfully.');
+        }
+        else {
+            console.log('Error:', response.status , response.message)
+        }
+    });
