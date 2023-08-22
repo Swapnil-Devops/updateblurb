e71239247d80c0b4a8fe0ad647fa83167211efc3
Swapnil Gawali <112171267+Swapnil-Devops@users.noreply.github.com>
2023-08-22 11:36:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import fetch from "node-fetch";
 import * as core from "@actions/core";
 
 const accessToken = core.getInput("PAT");
-const discussionId = "D_kwDOKCGw7s4AVF-r";
-const updatingbody = "GitHub Actions is a powerful automation and continuous integration/continuous deployment platform provided by GitHub. It allows you to automate various tasks and workflows in your software development process. Here's an explanation of GitHub Actions: Automation: GitHub Actions enables you to automate various tasks and processes related to your software development projects. This automation can include code validation, testing, building, deployment, and more. Workflow-Based: GitHub Actions is based on the concept of workflows. A workflow is a series of steps or jobs that are executed when specific events occur in your GitHub repository, such as code pushes, pull requests, or issue creation.";
+const discussionId = core.getInput("discussionId");
+const updatingbody = core.getInput("updatedbody");
 console.log('discussionId', discussionId);
 console.log('updatingbody', updatingbody);
 
